6bd6b67dd99e3e7874a45a1b27f7269f88c06201
Mason <Novakm@cbchs.org>
2025-08-25 10:02:01 -0500

    Famous quote Steven Wright

---
+++ famous_quotes_novak.py
@@ -0,0 +1,3 @@
+famous_person = ("-Steven Wright")
+message = ("\"Light Travels Faster Than Sound. This is why some people appear bright until you hear them speak.\"")
+print(message +"\n" + famous_person)
